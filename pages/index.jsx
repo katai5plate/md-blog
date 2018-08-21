828a118fa033e220724d9937e2f45233181b5af3
katai5plate <katai5plate.sub@gmail.com>
2018-08-22 00:45:12 +0900

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,12 @@ import Markdown from 'react-markdown';
 import articles from "../markdown/markdown.json";
 export default () => (
     <div>
+        <nav style={{
+            color: "white",
+            backgroundColor: "cyan"
+        }}>
+            <h1><a href="https://github.com/katai5plate/md-blog">GitHub Page</a></h1>
+        </nav>
         <Markdown source={articles[0].content} />
     </div>
 )
