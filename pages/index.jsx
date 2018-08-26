0d8791d49a970d59b01fea4eb279debf999da84e
katai5plate <katai5plate.sub@gmail.com>
2018-08-26 17:31:16 +0900

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,14 @@ import { Grid, Row, Col } from 'react-bootstrap';
 
 import Meta from '../components/Meta';
 import Header from '../components/Header';
+import ArticleList from '../components/ArticleList';
 
 export default () => (
   <div>
     <Meta />
     <Grid>
       <Header />
+      <ArticleList />
     </Grid>
   </div>
 )
