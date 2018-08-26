0c32314ba0a5380e36f7afe1d2c6e6dadc9e1210
katai5plate <katai5plate.sub@gmail.com>
2018-08-26 23:02:00 +0900

    add ogp

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -3,8 +3,21 @@ import Helmet from 'react-helmet';
 
 export default class Meta extends React.Component {
   render() {
+    const titleBar = "My Branding Page Prototype";
+    const ogType = {
+      website: "website",
+      blog: "blog",
+      article: "article",
+    }
     return (
       <Helmet
+        title={titleBar}
+        meta={[
+          { property: "og:title", content: titleBar },
+          { property: "og:type", content: ogType.website },
+          { property: "og:url", content: "https://md-blog-experiments.netlify.com" },
+          { property: "og:image", content: "static/image/logo.png" },
+        ]}
         link={[
           // Bootstrap
           {
